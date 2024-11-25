1d0a3bdd85b181682e549c32be0c2fd8f1dc0134
necrolingus <leigh.williams88@gmail.com>
2024-11-25 09:22:58 +0200

    removed static key

--- controller/config.js
+++ controller/config.js
@@ -1,7 +1,7 @@
 export const config = {
     port: process.env.OIU_PORT || 3000,
     uploadFolder: 'uploads',
-    uploadSecretValue: process.env.OIU_UPLOAD_SECRET || 'aaaaCVPOaaasdfsdfUUUUiodsfkndf@11111',
+    uploadSecretValue: process.env.OIU_UPLOAD_SECRET,
     deleteAfterDays: process.env.OIU_DELETE_AFTER_DAYS || 30,
     rl_window_minutes: process.env.OIU_RL_WINDOW_MINUTES || 3,
     rl_requests_in_window: process.env.OIU_RL_REQUESTS_IN_WINDOW || 20,
