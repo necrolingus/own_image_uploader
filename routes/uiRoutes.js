9e8676bfb4bbae41a8d24487162af8ef5535ed20
necrolingus <leigh.williams88@gmail.com>
2026-02-12 08:35:23 +0200

    delete after days default value is now the max value

--- routes/uiRoutes.js
+++ routes/uiRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import { config } from '../controller/config.js'
 
 const uiRouter = express.Router()
 
-uiRouter.get('/', async function(req, res) {
-    res.render('home');
+uiRouter.get('/', async function (req, res) {
+    const deleteAfterDaysMax = config.deleteAfterDaysMax || 100
+    res.render('home', { deleteAfterDaysMax });
 })
 
-export {uiRouter}
+export { uiRouter }
